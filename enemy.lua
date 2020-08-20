fe40eba42f12cb3cacb95f75c0179b063cc504fc
mobo <morgan.bohn@pc-scol.fr>
2020-08-20 19:19:55 +0200

    wip multiples skeletons

--- enemy.lua
+++ enemy.lua
@@ -1,47 +1,62 @@
+
+
 function initEnemies(map, world, enemiesLayer)
-    local enemy = {}
-    enemy.x = SPRITESIZE*2
-    enemy.y = SPRITESIZE*2
-    loadEnemy(world, enemiesLayer, enemy)
-    updateEnemy(world, enemiesLayer)
-    drawEnemy(enemiesLayer)
+    local enemies= {}
+    local enemy1 = {x = SPRITESIZE*2, y = SPRITESIZE*2}
+    local enemy2 = {x = SPRITESIZE*5, y = SPRITESIZE*7}
+    -- local enemy3 = {x = SPRITESIZE*2, y = SPRITESIZE*2}
+    table.insert(enemies, enemy1)
+    table.insert(enemies, enemy2)
+    -- table.insert(enemies, enemy3)
+
+
+   -- for index, enemy in ipairs(enemies) do
+    loadEnemies(world, enemiesLayer, enemies)
+    updateEnemies(world, enemiesLayer, enemies)
+    drawEnemies(enemiesLayer, enemies)
+
 end
 
 -- creer le jouer dans le layer
-function loadEnemy(world, layer, enemy)
-	local sprite = love.graphics.newImage(PATH_SKELETON)
-	local g = anim8.newGrid(SPRITESIZE, SPRITESIZE, sprite:getWidth(), sprite:getHeight())
-    layer.enemy = {
-		sprite = sprite,
-		animations = {
-            stop = anim8.newAnimation(g('1-2','1-1'), 0.5),
-            die = anim8.newAnimation(g('1-2','2-3'), 0.25, false),
-		},
-		x      = enemy.x,
-		y      = enemy.y,
-		width = SPRITESIZE,
-        height = SPRITESIZE,
-        speed = 50,
-        directionX = 1,
-        moves = {
-            stop = "stop",
-            run = "run",
-		},
-        life = 3,
-        dead = false
-    }
-    layer.enemy.animations.die.onLoop = function (anim)
-        anim:pauseAtEnd()
-        layer.enemy.dead = true
+function loadEnemies(world, layer, enemies)
+    enemiesLayer.enemies = {}
+    for index, enemy in ipairs(enemies) do
+        local sprite = love.graphics.newImage(PATH_SKELETON)
+        local g = anim8.newGrid(SPRITESIZE, SPRITESIZE, sprite:getWidth(), sprite:getHeight())
+        layer.enemies[index] = {
+            sprite = sprite,
+            animations = {
+                stop = anim8.newAnimation(g('1-2','1-1'), 0.5),
+                die = anim8.newAnimation(g('1-2','2-3'), 0.25),
+            },
+            x      = enemy.x,
+            y      = enemy.y,
+            width = SPRITESIZE,
+            height = SPRITESIZE,
+            speed = 50,
+            directionX = 1,
+            moves = {
+                stop = "stop",
+                run = "run",
+            },
+            life = 1,
+            dead = false,
+            removed = false
+        }
+        layer.enemies[index].animations.die.onLoop = function(anim)
+            anim:pauseAtEnd()
+            layer.enemies[index].dead = true
+        end
+
+
+        layer.enemies[index].animation = layer.enemies[index].animations.stop
+        layer.enemies[index].move = layer.enemies[index].moves.stop
+        world:add(layer.enemies[index], layer.enemies[index].x, layer.enemies[index].y, layer.enemies[index].width, layer.enemies[index].height)
+
     end
-
-
-    layer.enemy.animation = layer.enemy.animations.stop
-    layer.enemy.move = layer.enemy.moves.stop
-	world:add(layer.enemy, layer.enemy.x, layer.enemy.y, layer.enemy.width, layer.enemy.height)
 end
 
-function updateEnemy(world, layer)
+function updateEnemies(world, layer, enemies)
     local moveEnemy = function(enemy, goalX, goalY)
 		local actualX, actualY, cols, len = world:move(enemy, goalX, goalY)
 		enemy.x, enemy.y = actualX, actualY
@@ -54,45 +69,50 @@ function updateEnemy(world, layer)
 		end
     end
 
-    local continue = true
     layer.update = function(self, dt)
-        if continue then
-            -- remove the enemy from bump when he is deadd
-            if self.enemy.dead then
-                world:remove(self.enemy)
-                continue = false
+        for index, enemy in ipairs(enemies) do
+            print("ENEMY " .. index)
+            if not self.enemies[index].removed then
+                -- remove the enemy from bump when he is deadd
+                if self.enemies[index].dead then
+                    print("dead " .. index)
+                    world:remove(self.enemies[index])
+                    table.remove(enemies, index)
+                    self.enemies[index].removed = true
+                end
+
+                -- default animation
+                self.enemies[index].move = self.enemies[index].moves.run
+
+                -- die motherfucker !
+                if self.enemies[index].life <= 0 then
+                    self.enemies[index].animation = self.enemies[index].animations.die
+                    self.enemies[index].move = self.enemies[index].moves.stop
+                end
+
+                -- move enemy
+                if (self.enemies[index].move == self.enemies[index].moves.run) then
+                    moveEnemy(self.enemies[index], self.enemies[index].x + self.enemies[index].directionX * self.enemies[index].speed * dt, self.enemies[index].y)
+                end
+
+                -- On met à jour l'animation du joueur
+                self.enemies[index].animation:update(dt)
             end
-
-            -- default animation
-            layer.enemy.move = layer.enemy.moves.run
-
-            -- die motherfucker !
-            if self.enemy.life <= 0 then
-                print("DIE")
-                layer.enemy.animation = layer.enemy.animations.die
-                layer.enemy.move = layer.enemy.moves.stop
-            end
-
-            -- move enemy
-            if (layer.enemy.move == layer.enemy.moves.run) then
-                moveEnemy(self.enemy, self.enemy.x + self.enemy.directionX * self.enemy.speed * dt, self.enemy.y)
-            end
-
-            -- On met à jour l'animation du joueur
-            self.enemy.animation:update(dt)
         end
     end
 end
 
-function drawEnemy(layer)
+function drawEnemies(layer, enemies)
     layer.draw = function(self)
-        if not self.enemy.dead then
-            -- enemy's animation
-            self.enemy.animation:draw(self.enemy.sprite, math.floor(self.enemy.x), math.floor(self.enemy.y))
+        for index, enemy in ipairs(enemies) do
+            if not self.enemies[index].dead then
+                -- enemy's animation
+                self.enemies[index].animation:draw(self.enemies[index].sprite, math.floor(self.enemies[index].x), math.floor(self.enemies[index].y))
 
-            if ENABLE_DEBUG then
-                love.graphics.setPointSize(5)
-                love.graphics.points(math.floor(self.enemy.x), math.floor(self.enemy.y))
+                if ENABLE_DEBUG then
+                    love.graphics.setPointSize(5)
+                    love.graphics.points(math.floor(self.enemies[index].x), math.floor(self.enemies[index].y))
+                end
             end
         end
 	end
